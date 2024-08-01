d272e4a3ee7cdada34ab0fd4245c40f45d33dcca
Gonzalo Aguado Torres <gonagutor@gmail.com>
2024-08-01 14:34:42 +0200

    Add debounce to search to reduce redundant requests

--- providers/SearchProvider.tsx
+++ providers/SearchProvider.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import Character from "@/model/character";
 import useCharacters from "@/hooks/useCharacters";
+
+const DEBOUNCE_TIME = 1000;
 
 export const SearchContext = createContext<{
   term: string;
@@ -15,17 +17,23 @@ export const SearchContext = createContext<{
 export function SearchProvider({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const [lastTerm, setLastTerm] = useState("");
   const [term, setTerm] = useState("");
   const { error, fetch, loading, characters } = useCharacters();
 
-  const search = (term: string) => {
-    setTerm(term);
-    fetch(term);
-  };
+  useEffect(() => {
+    const searchOnTypingEnd = setTimeout(() => {
+      if (term != lastTerm) {
+        fetch(term);
+        setLastTerm(term);
+      }
+    }, DEBOUNCE_TIME);
+    return () => clearTimeout(searchOnTypingEnd);
+  }, [term]);
 
   return (
     <SearchContext.Provider
-      value={{ term, search, characters, error, loading }}
+      value={{ term, search: setTerm, characters, error, loading }}
     >
       {children}
     </SearchContext.Provider>
